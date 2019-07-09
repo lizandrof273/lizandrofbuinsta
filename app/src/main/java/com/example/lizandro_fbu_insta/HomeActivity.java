4ee1da70c38fefc528acec9a7de73f39d98278ae
Lizandro Franco <lizandro@fb.com>
2019-07-08 17:39:13 -0700

    i get the user posts now but i cannot disply them yet.

--- app/src/main/java/com/example/lizandro_fbu_insta/HomeActivity.java
+++ app/src/main/java/com/example/lizandro_fbu_insta/HomeActivity.java
@@ -1,20 +1,41 @@
 package com.example.lizandro_fbu_insta;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+
+import com.example.lizandro_fbu_insta.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); ++i) {
+                        Log.d("HomeActivity", "Post" + i + "] = "
+                                + objects.get(i).getDescription());
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+        /**
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -23,7 +44,7 @@ public class HomeActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
--- app/src/main/java/com/example/lizandro_fbu_insta/ParseApp.java
+++ app/src/main/java/com/example/lizandro_fbu_insta/ParseApp.java
@@ -2,12 +2,15 @@ package com.example.lizandro_fbu_insta;
 
 import android.app.Application;
 
+import com.example.lizandro_fbu_insta.model.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
         //getting started setting up parse server (initialzing)
         final Parse.Configuration  configuration =  new Parse.Configuration.Builder(this)
                 .applicationId("lizandro-instagram")
